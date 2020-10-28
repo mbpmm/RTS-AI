4c3cdce2e9b5f9ba760ea59fcc620673e1af4fa3
mbpmm <marianomcr92@gmail.com>
2020-10-28 03:04:46 -0300

    Ordenado de scripts, agregado game manager y MonoBehaviourSingleton script y prefab de minero

---
+++ Pathfinding/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviourSingleton<GameManager>
+{
+    public float gold;
+    public float minerCost;
+
+    public GameObject miner;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
